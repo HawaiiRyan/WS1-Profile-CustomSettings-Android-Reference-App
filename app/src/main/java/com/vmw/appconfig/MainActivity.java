c3cc0be2300bdf2e2c0036f9f67294a096d6ce10
Ryan Pringnitz <rpringnitz@gmail.com>
2022-02-26 12:29:51 -0500

    Update MainActivity.java
    Added comments in MainActivity.java

--- app/src/main/java/com/vmw/appconfig/MainActivity.java
+++ app/src/main/java/com/vmw/appconfig/MainActivity.java
@@ -16,12 +16,14 @@ public class MainActivity extends AppCompatActivity {
 
         this.appConfig = new AppConfig(getApplicationContext());
 
+        // System.out.println will print to Logcat
         System.out.println("Here is the Environment: " + appConfig.getEnvironment());
         System.out.println("Here is the URL: " + appConfig.getAddress());
         System.out.println("Here is the Example Value: " + appConfig.getAppConfigValue());
         System.out.println("Here is the Serial Number: " + appConfig.getSerialNumber());
         System.out.println("Here is the User: " + appConfig.getUser());
 
+        // Log.i will print to Logcat and device bugreports
         Log.i(AppConstants.LOG_TAG, "Here is the Environment: " + appConfig.getEnvironment());
         Log.i(AppConstants.LOG_TAG, "Here is the URL: " + appConfig.getAddress());
         Log.i(AppConstants.LOG_TAG, "Here is the Example Value: " + appConfig.getAppConfigValue());
@@ -35,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
         String toastExample = "Example Value: " + appConfig.getAppConfigValue();
         String toastUser = "User: " + appConfig.getUser();
 
+        // Toast messages will temporarily appear on the tablet, showing the values retrieved from
+        // Workspace ONE UEM Custom Settings profile payload
         Toast.makeText(getApplicationContext(),toastSerial, Toast.LENGTH_SHORT).show();
         Toast.makeText(getApplicationContext(),toastUrl, Toast.LENGTH_SHORT).show();
         Toast.makeText(getApplicationContext(),toastEnv, Toast.LENGTH_SHORT).show();
